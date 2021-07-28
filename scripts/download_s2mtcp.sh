c5a302eee2fa66f930409193a56d31395be399fa
Isaac Corley <22203655+isaaccorley@users.noreply.github.com>
2021-07-28 15:07:21 -0500

    updated download script

---
+++ scripts/download_s2mtcp.sh
@@ -0,0 +1,3 @@
+wget --no-check-certificate https://zenodo.org/record/4280482/files/S2MTCP_data.7z?download=1 -O S2MTCP_data.7z
+wget --no-check-certificate https://zenodo.org/record/4280482/files/S2MTCP_metadata.csv?download=1
+wget https://zenodo.org/record/4280482/files/README.txt?download=1 -O README.txt
