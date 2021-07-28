d77aef950e7c79524aed780533eb6d4befd0328d
Isaac Corley <22203655+isaaccorley@users.noreply.github.com>
2021-07-28 11:42:19 -0500

    update download script

--- scripts/download_s2looking.sh
+++ scripts/download_s2looking.sh
@@ -1,5 +1,3 @@
-apt-get install subversion
 mkdir -p .data/
-svn export https://github.com/AnonymousForACMMM/Dataset/trunk/S2Looking/S2Looking/train/ .data/s2looking/train/
-svn export https://github.com/AnonymousForACMMM/Dataset/trunk/S2Looking/S2Looking/val/ .data/s2looking/val/
-svn export https://github.com/AnonymousForACMMM/Dataset/trunk/S2Looking/S2Looking/test/ .data/s2looking/test/
+gdown --id 1M4w3acW0wGVuGFfxGxxZlw8lvIbAEa1I
+unzip S2Looking.zip 
