3e5fc197fafdd9cdb547afa237320eee00a97a62
Isaac Corley <22203655+isaaccorley@users.noreply.github.com>
2021-07-28 17:36:21 -0500

    updated download script

--- scripts/download_levircd_plus.sh
+++ scripts/download_levircd_plus.sh
@@ -1,5 +1,8 @@
 pip install gdown
-mkdir -p .data/levircd_plus
+mkdir -p .data/
 gdown --id 1JamSsxiytXdzAIk6VDVWfc-OsX-81U81
-unzip LEVIR-CD+.zip -d .data/levircd_plus
+unzip LEVIR-CD+.zip -d .data/
+mv .data/LEVIR-CD+ .data/levircd_plus
+rm -r .data/__MACOSX
+find .data/levircd_plus/ -name '.DS_Store' -type f -delete
 rm LEVIR-CD+.zip
