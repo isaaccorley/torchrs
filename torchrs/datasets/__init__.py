ee733552d9086048c59b28b9d730ecd07f0dc571
Isaac Corley <22203655+isaaccorley@users.noreply.github.com>
2021-07-29 16:40:12 -0500

    update

--- torchrs/datasets/__init__.py
+++ torchrs/datasets/__init__.py
@@ -5,9 +5,8 @@ from .eurosat import EuroSATRGB, EuroSATMS
 from .resisc45 import RESISC45
 from .rsicd import RSICD
 from .oscd import OSCD
-from .s2looking import S2Looking
 
 __all__ = [
     "PROBAV", "ETCI2021", "RSVQALR", "EuroSATRGB", "EuroSATMS",
-    "RESISC45", "RSICD", "OSCD", "S2Looking"
+    "RESISC45", "RSICD", "OSCD"
 ]
