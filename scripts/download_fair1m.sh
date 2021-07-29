1550eaa468ca3cdff1552c301b30ac0627da32b7
Isaac Corley <22203655+isaaccorley@users.noreply.github.com>
2021-07-29 17:45:06 -0500

    update

--- scripts/download_fair1m.sh
+++ scripts/download_fair1m.sh
@@ -1,2 +1,5 @@
-# Manually required to download dataset
-gdown --id https://drive.google.com/drive/folders/1-Z5HKienQy0MilMkt5JOuRzASLrUMfuJ?usp=sharing
+pip install gdown
+mkdir -p .data/fair1m
+gdown --id 1pmvfkC6xKQt8pO59_Nu6sBh4mf8MmRgw
+unzip FAIR1M.zip -d .data/fair1m
+rm FAIR1M.zip
