8cbd34e282b58a4407566236528c730363e3b0a1
Isaac Corley <22203655+isaaccorley@users.noreply.github.com>
2021-08-07 16:18:35 -0500

    finished datasets

--- torchrs/datasets/sat.py
+++ torchrs/datasets/sat.py
@@ -1,39 +1,70 @@
 import os
+from typing import Tuple
 
 import h5py
+import torch
 import torchvision.transforms as T
 
 from torchrs.transforms import ToTensor
 
 
-class SAT4(torch.utils.data.Dataset):
-    """ Image Captioning Dataset from 'Exploring Models and Data for
-    Remote Sensing Image Caption Generation', Lu et al. (2017)
-    https://arxiv.org/abs/1712.07835
-    """
-    classes = [
-        "barren land",
-        "trees",
-        "grassland",
-        "none"
-    ]
+class SAT(torch.utils.data.Dataset):
+    """ Base SAT dataset """
     def __init__(
         self,
-        root: str = ".data/sat",
+        path: str = "",
         split: str = "train",
         transform: T.Compose = T.Compose([ToTensor()])
     ):
         assert split in ["train", "test"]
-        self.path = os.path.join(root, "sat-4-full.mat")
+        self.path = path
+        self.split = split
         self.transform = transform
 
     def __len__(self) -> int:
         with h5py.File(self.path, "r") as f:
-            length = f.keys()[f"{split}_x"].shape[0]
+            length = f[f"{self.split}_y"].shape[0]
         return length
 
-    def __getitem__(self, idx: int) -> Dict:
-        captions = self.captions[idx]
+    def __getitem__(self, idx: int) -> Tuple[torch.Tensor, torch.Tensor]:
+        with h5py.File(self.path, "r") as f:
+            x = f[f"{self.split}_x"][idx]
+            y = f[f"{self.split}_y"][idx]
         x = self.transform(x)
-        sentences = [sentence["raw"] for sentence in captions["sentences"]]
-        return dict(x=x, captions=sentences)
+        y = torch.tensor(y).to(torch.long)
+        return x, y
+
+
+class SAT4(SAT):
+    """ Base SAT dataset """
+    classes = [
+        "barren land",
+        "trees",
+        "grassland",
+        "other"
+    ]
+    def __init__(
+        self,
+        path: str = ".data/sat/sat4.h5",
+        split: str = "train",
+        transform: T.Compose = T.Compose([ToTensor()])
+    ):
+        super().__init__(path, split, transform)
+
+
+class SAT6(SAT4):
+    classes = [
+        "barren land",
+        "trees",
+        "grassland",
+        "roads",
+        "buildings",
+        "water"
+    ]
+    def __init__(
+        self,
+        path: str = ".data/sat/sat6.h5",
+        split: str = "train",
+        transform: T.Compose = T.Compose([ToTensor()])
+    ):
+        super().__init__(path, split, transform)
