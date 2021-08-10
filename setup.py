19658096d4c19309561cc9ce58c8cca936a7f62e
Isaac Corley <22203655+isaaccorley@users.noreply.github.com>
2021-08-09 20:46:39 -0500

    updated reqs

--- setup.py
+++ setup.py
@@ -2,17 +2,18 @@ from setuptools import setup, find_packages
 from setuptools.config import read_configuration
 
 extras = {
-    "train": ["pytorch-lightning", "torchmetrics"],
+    "train": ["pytorch-lightning>=1.4.0", "torchmetrics>=0.4.1"],
 }
 install_requires = [
-    "torch",
-    "torchvision",
-    "torchaudio",
-    "einops",
-    "numpy",
-    "pillow",
-    "tifffile",
-    "h5py"
+    "torch>=1.9.0",
+    "torchvision>=0.10.0",
+    "torchaudio>=0.9.0",
+    "einops>=0.3.0",
+    "numpy>=1.21.0",
+    "pillow>=8.3.1",
+    "tifffile>=2021.7.2",
+    "h5py>=3.3.0",
+    "imagecodecs>=2021.7.30"
 ]
 setup_requires = ["pytest-runner"]
 tests_require = ["pytest", "pytest-cov", "mock", "mypy", "black", "pylint"]
