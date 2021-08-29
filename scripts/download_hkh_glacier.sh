49bc6cf78569f77f02ad766868b679b0d0b1343d
Isaac Corley <22203655+isaaccorley@users.noreply.github.com>
2021-08-29 14:35:37 -0500

    updated hkh drive link

--- scripts/download_hkh_glacier.sh
+++ scripts/download_hkh_glacier.sh
@@ -1,5 +1,5 @@
 pip install gdown
 mkdir -p .data
-gdown --id 1cvjfe_MZJI9HXwkRgoQbSCH55qQd6Esm
+gdown --id 1Tk32aGadb7YBfI5JxLbX6kXUGdJNiC8p
 unzip hkh_glacier_mapping.zip -d .data/
 rm hkh_glacier_mapping.zip
